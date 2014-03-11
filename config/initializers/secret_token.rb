ec1d7c7b2a93cfce8532602a47d7bc1a4d2918eb
Trisha Hadley <trisha.hadley@gmail.com>
2014-03-10 19:15:19 -0500

    First commit and README update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccitoff::Application.config.secret_token = 'dbb0507d007b109617a6a6d6ec704199fec051bd8018a8c39d382bca68e001b20f3baeca592dcb57e2439402c4d08b47bd94cb48dc3e405c83cf953a9bc468b1'
