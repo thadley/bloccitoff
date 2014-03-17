e3199540dc0d183077d39f6953000f6ac24da3b5
Trisha Hadley <trisha.hadley@gmail.com>
2014-03-17 10:54:15 -0500

    Update to rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloccitoff::Application.config.secret_key_base = 'cd6d29413d7cb4c8c912e786309a29a1f27d846ad81c7bfdd0c28e89623e526a439c428a598437743462d09eba5861af406605b3f076afea7ad7c0cd2c27cda9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bloccitoff::Application.config.session_store :cookie_store, key: '_bloccitoff_session'
