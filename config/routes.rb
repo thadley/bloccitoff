778af09fccfc4f72d7d83d24c05f03af977301f3
Trisha Hadley <trisha.hadley@gmail.com>
2014-03-20 11:40:44 -0500

    Created todos index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Bloccitoff::Application.routes.draw do
   devise_for :users
   resources :users, only: [:update]
-  resources :todos, only: [:new, :create, :show]
+  resources :todos, only: [:new, :create, :show, :index]
 
   get "welcome/about"
   
