f111247c0fedcf59f612907409cb44fa7abf05fa
Trisha Hadley <trisha.hadley@gmail.com>
2014-03-22 00:34:56 -0500

    Associated users and todos, created seed data with Faker gem

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,7 @@
 class Todo < ActiveRecord::Base
   
+  belongs_to :user
+
   validates :description, length: { minimum: 2 }, presence: true
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create 5 users with their own todos
+5.times do
+  password = Faker::Lorem.characters(10)
+  user = User.new(
+    name: Faker::Name.name, 
+    email: Faker::Internet.email, 
+    password: password, 
+    password_confirmation: password)
+  user.skip_confirmation!
+  user.save
+
+  # Note: by calling `User.new` instead of `create`,
+  # we create an instance of a user which isn't saved to the database.
+  # The `skip_confirmation!` method sets the confirmation date
+  # to avoid sending an email. The `save` method updates the database.
+
+  5.times do
+    todo = Todo.create(
+      user: user,
+      description: Faker::Lorem.sentence)
+    # set the created_at to a time within the past week
+    todo.update_attribute(:created_at, Time.now - rand(600..606461))
+  end
+end
+
+
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes(email: 'trisha.hadley@gmail.com', password: 'helloworld', password_confirmation: 'helloworld')
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Todo.count} todos created"
