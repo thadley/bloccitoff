bdc6a02e54d4d23b7df3b72f4d81ecd2db73e59e
Trisha Hadley <trisha.hadley@gmail.com>
2014-04-02 17:24:23 -0500

    Completed capybara testing

--- spec/features/project_manager_checks_todo_complete_spec.rb
+++ spec/features/project_manager_checks_todo_complete_spec.rb
@@ -8,37 +8,43 @@ user.confirmed_at = Time.now
 user.save
 
 feature 'PM checks todo as complete to delete it', js: true do
-    scenario 'Successfully' do
+    before do
         login_as(user, :scope => :user)
         visit new_todo_path
         fill_in 'Description', with: 'Meet up with the team'
         click_button 'Save'
         expect( page ).to have_content('Your new todo was saved')
         expect( page ).to have_content('Meet up with the team')
+    end
+
+    scenario 'Successfully' do
         find(:css, ".todoCheckbox").click
         page.driver.browser.switch_to.alert.accept
         expect( page ).to have_no_content('Meet up with the team')
     end
 
     scenario 'cancels delete' do
-        login_as(user, :scope => :user)
-        
+        find(:css, ".todoCheckbox").click
+        page.driver.browser.switch_to.alert.dismiss
+        expect( page ).to have_content('Meet up with the team')
     end
 end
 
 Warden.test_reset!
 
+# Feature: PM checks todo as complete to delete it
+
 # Scenario: Successfully
 
-# PM goes to the todo index list page
-# PM selects check box next to todo item
-# When checked, PM confirms delete 
-# Todo item is deleted from list
-# PM sees message that todo is complete
+#   PM goes to the todo index list page
+#   PM selects check box next to todo item
+#   When checked, PM confirms delete 
+#   Todo item is deleted from list
+#   PM sees message that todo is complete
 
 # Scenario: cancels delete
 
-# PM goes to the todo index list page
-# PM selects check box next to todo item
-# When checked, PM cancels delete
-# Checkbox is unchecked, todo item remains on list
+#   PM goes to the todo index list page
+#   PM selects check box next to todo item
+#   When checked, PM cancels delete
+#   Checkbox is unchecked, todo item remains on list
